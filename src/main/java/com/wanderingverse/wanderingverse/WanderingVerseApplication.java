fe888251e3d2cd3110b0c7608eef5cb34241e361
Snow Ball <1910058487@qq.com>
2025-05-06 17:49:50 +0800

    project init

---
+++ src/main/java/com/wanderingverse/wanderingverse/WanderingVerseApplication.java
@@ -0,0 +1,16 @@
+package com.wanderingverse.wanderingverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author lihui
+ * @date 2025/05/06 17:45
+ */
+@SpringBootApplication
+public class WanderingVerseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WanderingVerseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wanderingverse/wanderingverse/WanderingVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderingverse.wanderingverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderingVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
