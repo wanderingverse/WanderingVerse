2b4d9aecef1abeaed99a3881b226be45925eb201
Snow Ball <1910058487@qq.com>
2025-05-07 16:25:14 +0800

    project init

--- src/main/java/com/wanderingverse/wanderingverse/WanderingVerseApplication.java
+++ src/main/java/com/wanderingverse/wanderingverse/WanderingVerseApplication.java
@@ -1,5 +1,6 @@
 package com.wanderingverse.wanderingverse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,17 +10,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2025/05/06 17:45
  */
 @SpringBootApplication
+@Slf4j
 public class WanderingVerseApplication {
     public static void main(String[] args) {
         SpringApplication.run(WanderingVerseApplication.class, args);
-        System.out.println("""
-                                   _    _                 _           _             _   _                   \s
-                                   | |  | |               | |         (_)           | | | |                  \s
-                                   | |  | | __ _ _ __   __| | ___ _ __ _ _ __   __ _| | | | ___ _ __ ___  ___\s
-                                   | |/\\| |/ _` | '_ \\ / _` |/ _ \\ '__| | '_ \\ / _` | | | |/ _ \\ '__/ __|/ _ \\
-                                   \\  /\\  / (_| | | | | (_| |  __/ |  | | | | | (_| \\ \\_/ /  __/ |  \\__ \\  __/
-                                    \\/  \\/ \\__,_|_| |_|\\__,_|\\___|_|  |_|_| |_|\\__, |\\___/ \\___|_|  |___/\\___|
-                                                                                __/ |                        \s
-                                                                               |___/""");
+        log.info("""
+
+                          _    _                 _           _             _   _                   \s
+                         | |  | |               | |         (_)           | | | |                  \s
+                         | |  | | __ _ _ __   __| | ___ _ __ _ _ __   __ _| | | | ___ _ __ ___  ___\s
+                         | |/\\| |/ _` | '_ \\ / _` |/ _ \\ '__| | '_ \\ / _` | | | |/ _ \\ '__/ __|/ _ \\
+                         \\  /\\  / (_| | | | | (_| |  __/ |  | | | | | (_| \\ \\_/ /  __/ |  \\__ \\  __/
+                          \\/  \\/ \\__,_|_| |_|\\__,_|\\___|_|  |_|_| |_|\\__, |\\___/ \\___|_|  |___/\\___|
+                                                                      __/ |                        \s
+                                                                     |___/                         \s
+                         """);
     }
 }
