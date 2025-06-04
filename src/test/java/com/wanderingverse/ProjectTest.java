30ed52db6e773ddb5f6e5588b993caed7935dc8a
wanderingverse <lh19890781942@126.com>
2025-06-04 18:16:10 +0800

    2025年06月04日

--- src/test/java/com/wanderingverse/ProjectTest.java
+++ src/test/java/com/wanderingverse/ProjectTest.java
@@ -2,19 +2,13 @@ package com.wanderingverse;
 
 
 import lombok.extern.slf4j.Slf4j;
-import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Locale;
 
 @Slf4j
 @SpringBootTest
 class ProjectTest {
-
-
     @Test
-    public void projectTest() throws InterruptedException {
-        Faker faker = new Faker(Locale.forLanguageTag("zh-CN"));
+    public void projectTest() {
     }
 }
