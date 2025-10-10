5b0d05b76c60e97dc4230ea3034e078a0d1c07ce
wanderingverse <lh19890781942@126.com>
2025-10-10 17:33:48 +0800

    - 优化项目启动时日志输出和日志文件归档方式

--- src/main/java/com/wanderingverse/WanderingVerseApplication.java
+++ src/main/java/com/wanderingverse/WanderingVerseApplication.java
@@ -16,16 +16,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WanderingVerseApplication {
     public static void main(String[] args) {
         SpringApplication.run(WanderingVerseApplication.class, args);
-        log.info("""
-
-                          _    _                 _           _             _   _                   \s
-                         | |  | |               | |         (_)           | | | |                  \s
-                         | |  | | __ _ _ __   __| | ___ _ __ _ _ __   __ _| | | | ___ _ __ ___  ___\s
-                         | |/\\| |/ _` | '_ \\ / _` |/ _ \\ '__| | '_ \\ / _` | | | |/ _ \\ '__/ __|/ _ \\
-                         \\  /\\  / (_| | | | | (_| |  __/ |  | | | | | (_| \\ \\_/ /  __/ |  \\__ \\  __/
-                          \\/  \\/ \\__,_|_| |_|\\__,_|\\___|_|  |_|_| |_|\\__, |\\___/ \\___|_|  |___/\\___|
-                                                                      __/ |                        \s
-                                                                     |___/                         \s
-                         """);
     }
 }
--- src/main/java/com/wanderingverse/config/StartupLogger.java
+++ src/main/java/com/wanderingverse/config/StartupLogger.java
@@ -4,8 +4,9 @@ import com.wanderingverse.mapper.systemmapper.VersionMapper;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -20,39 +21,48 @@ import java.util.Arrays;
  */
 @Slf4j
 @Component
-public class StartupLogger implements ApplicationRunner {
-    private final long maxMemory = Runtime.getRuntime().maxMemory() / (1024 * 1024);
-    private final long totalMemory = Runtime.getRuntime().totalMemory() / (1024 * 1024);
-    private final long freeMemory = Runtime.getRuntime().freeMemory() / (1024 * 1024);
+public class StartupLogger {
     @Resource
     private Environment environment;
     @Resource
     private VersionMapper versionMapper;
 
-    @Override
-    public void run(ApplicationArguments args) {
-        log.info("============================================================");
-        log.info("                      系统启动信息");
-        log.info("============================================================");
-        log.info("【系统信息】");
-        log.info("  操作系统：         {} {}", System.getProperty("os.name"), System.getProperty("os.version"));
-        log.info("  用户名：           {}", System.getProperty("user.name"));
-        log.info("  用户目录：         {}", System.getProperty("user.home"));
-        log.info("  工作目录：         {}", System.getProperty("user.dir"));
-        log.info("------------------------------------------------------------");
-        log.info("【Java 信息】");
-        log.info("  Java版本：         {}", System.getProperty("java.version"));
-        log.info("  Java路径：         {}", System.getProperty("java.home"));
-        log.info("  命令行参数：        {}", args.getOptionNames());
-        log.info("  运行环境：          {}", Arrays.toString(environment.getActiveProfiles()));
-        log.info("  JVM 最大可用内存：   {} MB", maxMemory);
-        log.info("  JVM 已分配内存：     {} MB", totalMemory);
-        log.info("  JVM 空闲内存：      {} MB", freeMemory);
-        log.info("------------------------------------------------------------");
-        log.info("【服务信息】");
-        log.info("  Spring Boot版本：  {}", SpringBootVersion.getVersion());
-        log.info("  MySQL 版本：       {}", versionMapper.getDataBaseVersion());
-        log.info("  MySQL URL：       {}", environment.getProperty("spring.datasource.url"));
-        log.info("============================================================");
+    @EventListener(ApplicationReadyEvent.class)
+    public void showStartupLog(ApplicationReadyEvent applicationReadyEvent) {
+        ApplicationArguments applicationArguments = applicationReadyEvent.getApplicationContext().getBean(ApplicationArguments.class);
+        long maxMemory = Runtime.getRuntime().maxMemory() / (1024 * 1024);
+        long totalMemory = Runtime.getRuntime().totalMemory() / (1024 * 1024);
+        long freeMemory = Runtime.getRuntime().freeMemory() / (1024 * 1024);
+        String line = "-------------------------------------------------------------------------------";
+        log.info(line);
+        log.info(String.format("%-20s : %s %s", "Operating System", System.getProperty("os.name"), System.getProperty("os.version")));
+        log.info(String.format("%-20s : %s", "Username", System.getProperty("user.name")));
+        log.info(String.format("%-20s : %s", "User Home", System.getProperty("user.home")));
+        log.info(String.format("%-20s : %s", "Working Directory", System.getProperty("user.dir")));
+        log.info(line);
+        log.info(String.format("%-20s : %s", "Java Version", System.getProperty("java.version")));
+        log.info(String.format("%-20s : %s", "Java Home", System.getProperty("java.home")));
+        log.info(String.format("%-20s : %s", "JVM Args", Arrays.toString(applicationArguments.getSourceArgs())));
+        log.info(String.format("%-20s : %s", "Active Profiles", Arrays.toString(environment.getActiveProfiles())));
+        log.info(String.format("%-20s : %d MB", "JVM Max Memory", maxMemory));
+        log.info(String.format("%-20s : %d MB", "JVM Total Memory", totalMemory));
+        log.info(String.format("%-20s : %d MB", "JVM Free Memory", freeMemory));
+        log.info(line);
+        log.info(String.format("%-20s : %s", "Spring Boot Version", SpringBootVersion.getVersion()));
+        log.info(String.format("%-20s : %s", "MySQL Version", versionMapper.getDataBaseVersion()));
+        log.info(String.format("%-20s : %s", "MySQL URL", environment.getProperty("spring.datasource.url")));
+        log.info(line);
+        log.info("""
+                         The application has started successfully.
+                          _    _                 _           _             _   _                   \s
+                         | |  | |               | |         (_)           | | | |                  \s
+                         | |  | | __ _ _ __   __| | ___ _ __ _ _ __   __ _| | | | ___ _ __ ___  ___\s
+                         | |/\\| |/ _` | '_ \\ / _` |/ _ \\ '__| | '_ \\ / _` | | | |/ _ \\ '__/ __|/ _ \\
+                         \\  /\\  / (_| | | | | (_| |  __/ |  | | | | | (_| \\ \\_/ /  __/ |  \\__ \\  __/
+                          \\/  \\/ \\__,_|_| |_|\\__,_|\\___|_|  |_|_| |_|\\__, |\\___/ \\___|_|  |___/\\___|
+                                                                      __/ |                        \s
+                                                                     |___/                         \s
+                         """);
+        log.info(line);
     }
 }
