400c61bc34d88f6539bd68c60e2b7f0b98ce3bfd
wanderingverse <lh19890781942@126.com>
2025-10-22 16:27:16 +0800

    - 测试算法工具类

--- src/test/java/com/wanderingverse/algorithms/Algorithm.java
+++ src/test/java/com/wanderingverse/algorithms/Algorithm.java
@@ -1,11 +1,35 @@
 package com.wanderingverse.algorithms;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author lihui
  * @since 2025/9/25 16:47
  **/
+@Slf4j
 public class Algorithm {
     public static void main(String[] args) {
+        System.out.println(new Algorithm().sumRangeRecursive(1, 10000));
+    }
 
+
+    /**
+     * 累加从 start 到 end 的和
+     * <p>递归法
+     *
+     * @param start long
+     * @param end   long
+     * @return long
+     */
+    public long sumRangeRecursive(long start, long end) {
+        log.info("start: {}, end: {}", start, end);
+        if (start > end) {
+            return 0;
+        }
+        if (start == end) {
+            return end;
+        } else {
+            return start + sumRangeRecursive(start + 1, end);
+        }
     }
 }
