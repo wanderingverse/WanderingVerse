82330ddba72dcaf37997b2b619fbd9a88d8cac86
wanderingverse <lh19890781942@126.com>
2025-11-10 22:00:22 +0800

    - 集成 RAG

--- src/main/java/com/wanderingverse/common/AiCommon.java
+++ src/main/java/com/wanderingverse/common/AiCommon.java
@@ -10,4 +10,15 @@ public class AiCommon {
      * 最大会话记录保存数
      */
     public static final int MAX_MESSAGES = 32;
+
+    /**
+     * 余弦相似度
+     * between [0,1]
+     */
+    public static final double COSINE_SIMILARITY = 0.5;
+
+    /**
+     * 最大 RAG 检索结果数
+     */
+    public static final int MAX_RESULT_NUMBER_FOR_RAG = 10;
 }
--- src/main/java/com/wanderingverse/config/AiConfig.java
+++ src/main/java/com/wanderingverse/config/AiConfig.java
@@ -3,9 +3,13 @@ package com.wanderingverse.config;
 import com.wanderingverse.repository.RedisChatMemoryStore;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-import static com.wanderingverse.common.AiCommon.MAX_MESSAGES;
+import static com.wanderingverse.common.AiCommon.*;
 
 /**
  * @author lihui
@@ -36,11 +40,27 @@ public class AiConfig {
     }
 
     @Bean
-    public EmbeddingStore embeddingStore() {
+    public EmbeddingStore<TextSegment> embeddingStore() {
         // 从 resource 路径指定目录下加载所有文件（包括子目录中文件）并对应为文档（Document）
-        List<Document> documentList = ClassPathDocumentLoader.loadDocumentsRecursively("./document");
-        // 初始化向量数据库操作对象，用于操作 langchain4j-easy-rag 提供的、基于内存的向量数据库
-        InMemoryEmbeddingStore inMemoryEmbeddingStore = new InMemoryEmbeddingStore();
+        List<Document> documentList = ClassPathDocumentLoader.loadDocumentsRecursively("document");
+        // 初始化向量数据库操作对象，用于操作 langchain4j-easy-rag 提供的基于内存的向量数据库
+        InMemoryEmbeddingStore<TextSegment> inMemoryEmbeddingStore = new InMemoryEmbeddingStore<>();
+        // 文档内容分割、向量化、存储到向量数据库
+        EmbeddingStoreIngestor embeddingStoreIngestor = EmbeddingStoreIngestor.builder()
+                                                                              .embeddingStore(inMemoryEmbeddingStore)
+                                                                              .build();
+        embeddingStoreIngestor.ingest(documentList);
         return inMemoryEmbeddingStore;
+    }
+
+    @Bean
+    public ContentRetriever contentRetriever(EmbeddingStore<TextSegment> embeddingStore) {
+        return EmbeddingStoreContentRetriever.builder()
+                                             .embeddingStore(embeddingStore)
+                                             // 余弦相似度，[0,1]
+                                             .minScore(COSINE_SIMILARITY)
+                                             // 最大返回结果数
+                                             .maxResults(MAX_RESULT_NUMBER_FOR_RAG)
+                                             .build();
     }
 }
